709a715785e8d56e10de52efb0c9e2147b13b372
glitch-in-the-herring <70984234+glitch-in-the-herring@users.noreply.github.com>
2022-08-08 13:43:21 +0700

    why is it corrupted

--- main.js
+++ main.js
@@ -76,7 +76,7 @@ function on_file_open()
         show_skills(inv_e, char_e, slots[0].inv);
         inv_e.inv_label.textContent = "Item";
         inv_e.inv_next_button.removeAttribute("disabled");
-        show_party(party_e, slots[0]);
+        //show_party(party_e, slots[0]);
 
         if (addresses.length > 1)
             next_buttons.forEach(x => x.removeAttribute("disabled"));
